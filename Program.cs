051558e9cb49a134deba6c00f81661a5e89a675d
mohammed3599 <kataloni1999@gmail.com>
2023-07-24 10:26:49 +0400

    push, pop, and peek

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack Stack = new Stack(5);
+            Stack.Push(10);
+            Stack.Push(20);
+            Stack.Push(30);
+            Stack.Push(40);
+            Stack.Push(50);
+            Stack.Push(60);
+            Console.WriteLine(Stack.Pop());
+            Console.WriteLine(Stack.peek());
+            Console.WriteLine(Stack.Pop());
+            Stack.Push(60);
+            Console.WriteLine(Stack.peek());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        private int[] stack;
+        private int top;
+
+        public Stack(int n)
+        {
+            this.stack = new int[n];
+            top = -1;
+        }
+
+        public void Push(int number)
+        {
+            if (top >= stack.Length - 1)
+            {
+                Console.WriteLine("Stack is full");
+                return;
+            }
+            stack[++top] = number;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            return stack[top--];
+        }
+
+        public int peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            return stack[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        private bool IsFull()
+        {
+            return top == 0;
+        }
+
+
+    }
+}
